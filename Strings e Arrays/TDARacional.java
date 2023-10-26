88433f07052a15e0e6b1a55e7486eb4c318b289f
Luiz Furlanis <furlanis.lu@gmail.com>
2023-10-26 20:00:28 -0300

    teste de funcionamento commit

--- Strings e Arrays/TDARacional.java
+++ Strings e Arrays/TDARacional.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class TDARacional {
     public static void main(String[] args) throws IOException{
         Scanner teclado = new Scanner(System.in);
+        // funciona por favor
         teclado.close();
     }
 }
