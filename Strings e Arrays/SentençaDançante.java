9360aec118ffe7f3ba619e32bfc6119139ee5e45
Luiz Furlanis <furlanis.lu@gmail.com>
2023-10-26 20:02:57 -0300

    A

--- Strings e Arrays/SentençaDançante.java
+++ Strings e Arrays/SentençaDançante.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class SentençaDançante {
     public static void main(String[] args) throws IOException{
         Scanner teclado = new Scanner(System.in);
+        // rapa casa do carai
         teclado.close();
     }
 }
