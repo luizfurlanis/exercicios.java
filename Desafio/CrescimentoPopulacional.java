43187357723b78bc02407134f74ad8694a19b057
luizfurlanis <furlanis.lu@gmail.com>
2023-11-22 14:22:30 -0300

    commit

---
+++ Desafio/CrescimentoPopulacional.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class CrescimentoPopulacional {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
---
+++ Desafio/RelogioAntigo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class RelogioAntigo {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
