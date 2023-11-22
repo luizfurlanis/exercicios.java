66924c379640b5f09148c937227ba6635c76b49a
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2023-11-22 19:19:49 -0300

    commit

--- Desafio/RelogioAntigo.java
+++ Desafio/RelogioAntigo.java
@@ -4,5 +4,28 @@ import java.io.IOException;
 public class RelogioAntigo {
     public static void main(String[] args) throws IOException {
         Scanner teclado = new Scanner(System.in);
+        int hora;
+        int minuto;
+        int horaFinal;
+        int minutoFinal;
+
+        for(int i = 0; i < hora; i += 15){
+            horaFinal += 1;
+        }
+        for(int i = 0; i < minuto; i += 6){
+            minutoFinal += 1;
+        }
+
+        if(horaFinal < 10){
+            Sistem.out.println("0" + horaFinal);
+        } else {
+            Sistem.out.println(horaFinal);
+        }
+
+        if(minutoFinal < 10){
+            Sistem.out.println("0" + minutoFinal);
+        } else {
+            Sistem.out.println(minutoFinal);
+        }
     }
 }
--- Desafio/TempoMinutos.java
+++ Desafio/TempoMinutos.java
@@ -4,28 +4,5 @@ import java.io.IOException;
 public class TempoMinutos {
     public static void main(String[] args) throws IOException {
         Scanner teclado = new Scanner(System.in);
-        int hora;
-        int minuto;
-        int horaFinal;
-        int minutoFinal;
-
-        for(int i = 0; i < hora; i += 15){
-            horaFinal += 1;
-        }
-        for(int i = 0; i < minuto; i += 6){
-            minutoFinal += 1;
-        }
-
-        if(horaFinal < 10){
-            Sistem.out.println("0" + horaFinal);
-        } else {
-            Sistem.out.println(horaFinal);
-        }
-
-        if(minutoFinal < 10){
-            Sistem.out.println("0" + minutoFinal);
-        } else {
-            Sistem.out.println(minutoFinal);
-        }
     }
 }
